2dd432a920854fae9378ca14675e023044cf1bec
mocnyjakub <jmocny@edu.cdv.pl>
2021-06-25 18:32:27 +0200

    Adding initial files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { AddTodoComponent } from './add-todo/add-todo.component';
+import { ButtonComponent } from './button/button.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, AddTodoComponent, ButtonComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
